af537e8c5b54f9ff29b9e8ca79056e7eb776e3ed
M <mtso@tuta.io>
2017-06-27 14:07:32 -0700

    Document bump.js

--- src/bump.js
+++ src/bump.js
@@ -1,30 +1,46 @@
 // #!/usr/bin/env node
+// bump is a script to increment the version string in a package.json file.
+// Assumes the semver format: [major].[minor].[patch]
+//
+// Usage:
+// $ bump [major|minor|patch]
 
-const packageFile = './package.json' // path relative to command invocation
+'use strict';
+
+const fs = require('fs');
+
+// Assume we are running in the root directory of the package
+// (relative path to package.json from command invocation).
+const packageFile = './package.json';
+
+// Maps parts to the index of an array containing the strings split by '.'
 const partTable = {
   major: 0,
   minor: 1,
   patch: 2,
-}
-
-const fs = require('fs');
+};
 
 fs.readFile(packageFile, parseInfo(function(info) {
+  // Save string for rendering result.
   const previous = info.version;
   const version = info.version.split('.');
 
   const args = process.argv;
+  
+  // Take the last argument in the command to determine part.
   const partArg = args[args.length - 1];
   var part = partTable[partArg.toLowerCase()];
   
+  // Default to the patch part of the version if none is given.
   if (part === undefined) {
     part = 2;
   }
 
-  // Bump.
+  // Increment part.
   version[part] = +version[part] + 1;
   info.version = version.join('.');
 
+  // Overwrite package.json with the new version.
   fs.writeFile(packageFile, JSON.stringify(info, null, 2), {flag: 'w'},
     function(err) {
       if (err) {
@@ -37,11 +53,13 @@ fs.readFile(packageFile, parseInfo(function(info) {
 
 // Helper funcs:
 
+// parseInfo parses the read data into JSON.
 function parseInfo(callback) {
   return function(err, data) {
     if (err) {
       throw err;
     }
+    // TODO: wrap in a try-catch block.
     const info = JSON.parse(data.toString());
     callback(info);
   }
