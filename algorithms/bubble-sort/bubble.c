f8747a0e5faa378cd7e9ef64f6c8f968d0afea72
mtso <mtso@tuta.io>
2017-07-06 23:56:43 -0700

    Stub bubble check()

---
+++ algorithms/bubble-sort/bubble.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int check(int want, int got)
+{
+    if (want != got)
+    {
+        printf("FAIL %s:%d: %d != %d\n", __FILE__, __LINE__, want, got);
+        return 0;
+    }
+    return 1;
+}
+
+const int SIZE = 10;
+
+int main()
+{
+    int arr[SIZE] = {1, 2, 3, 4};
+
+    if (!check(1, 2)) {
+        return 1;
+    }
+
+    return 0;
+}
