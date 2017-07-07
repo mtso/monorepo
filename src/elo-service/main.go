1f756bdf918207e959e59645006aca0a9b7c3c9d
mtso <mtso@tuta.io>
2017-07-07 12:17:13 -0700

    Stub elo-service folder

---
+++ src/elo-service/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello~"))
+	})
+	http.ListenAndServe(":3750", nil)
+}
