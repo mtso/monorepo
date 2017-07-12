48c10c3971b34faddefcaec3cc5477dc6d6b8197
mtso <mtso@tuta.io>
2017-07-11 22:15:30 -0700

    Complete bubble_sort in c

--- algorithms/bubble-sort/bubble.c
+++ algorithms/bubble-sort/bubble.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/* check is an assertion function for two ints. */
 int check(int want, int got) {
     if (want != got) {
         printf("FAIL %s:%d: %d != %d\n", __FILE__, __LINE__, want, got);
@@ -8,14 +9,58 @@ int check(int want, int got) {
     return 1;
 }
 
-const int SIZE = 10;
+/* swap swaps the values that a and b point to. */
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/* bubble_sort iteratively compares the current value
+ * and the next value to sort an array in O(n^2) */
+void bubble_sort(
+    int *nums,
+    int numlen,
+    int (*comparer)(int, int)
+) {
+    for (int i = 0; i < numlen-1; ++i) {
+        for (int j = 0; j < numlen-1; ++j) {
+            if ((*comparer)(*(nums+j), *(nums+j+1))){
+                swap((nums+j), (nums+j+1));
+            }
+        }
+    }
+}
+
+/* printarray outputs an array of ints to stdio. */
+void printarray(int *nums, int numlen) {
+    for (int i = 0; i < numlen; ++i) {
+        printf("%d ", *(nums+i));
+    }
+}
+
+/* asc compares a to b for ascending order. */
+int asc(int a, int b) {
+    return a > b;
+}
+
+/* desc compares a to b for descending order. */
+int desc(int a, int b) {
+    return a < b;
+}
+
+const int SIZE = 5;
 
 int main() {
-    int arr[SIZE] = {1, 2, 3, 4};
+    int arr[SIZE] = {1, 7, 3, 2, 1};
 
-    if (!check(1, 2)) {
-        return 1;
-    }
+    printarray(arr, SIZE);
+    printf("\n");
+
+    bubble_sort(arr, SIZE, desc);
+
+    printarray(arr, SIZE);
+    printf("\n");
 
     return 0;
 }
