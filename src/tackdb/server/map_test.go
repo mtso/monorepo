b69bef125b9a49eae62357bf628b1ecf81c72bba
mtso <mtso@tuta.io>
2017-07-13 21:42:45 -0700

    Test two clients sharing data

---
+++ src/tackdb/server/map_test.go
@@ -0,0 +1,78 @@
+package server
+
+import (
+	"bufio"
+	"errors"
+	"net"
+	"testing"
+	"time"
+)
+
+var ErrTimeout = errors.New("timed out")
+
+func TestMap(t *testing.T) {
+	port := "3750"
+	timeout := time.NewTimer(1000)
+	want := "bar"
+	done := make(chan error)
+	close := make(chan error)
+	server := &Server{}
+
+	go func() {
+		err := server.Start(port)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer server.Close()
+
+		done <- <-close
+	}()
+
+	go func() {
+		client1, err := net.Dial("tcp", "127.0.0.1:"+port)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer client1.Close()
+
+		client1.Write([]byte("SET foo " + want))
+	}()
+
+	go func() {
+		client2, err := net.Dial("tcp", "127.0.0.1:"+port)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer client2.Close()
+
+		time.Sleep(100)
+		client2.Write([]byte("GET foo"))
+
+		msg, err := bufio.NewReader(client2).ReadString('\n')
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if msg != want {
+			t.Errorf("Expected the response of \"GET foo\" == %s, but got %s", want, msg)
+		}
+		close <- nil
+	}()
+
+	go func() {
+		select {
+		case <-timeout.C:
+			close <- ErrTimeout
+		}
+	}()
+
+	err := <-done
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	isStopped := timeout.Stop()
+	if !isStopped {
+		t.Errorf("timeout should not have expired.")
+	}
+}
