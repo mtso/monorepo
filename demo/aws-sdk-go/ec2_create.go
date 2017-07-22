6f6d995061da3d80eb8069fe200145f906c17f51
mtso <mtso@tuta.io>
2017-07-22 10:12:37 -0700

    Add start script user data

--- demo/aws-sdk-go/ec2_create.go
+++ demo/aws-sdk-go/ec2_create.go
@@ -1,8 +1,11 @@
 package main
+
 // ami-ad5da9e9
 // ami-86f4dbe6
+// ami-f4331a94
 
 import (
+	"encoding/base64"
 	"log"
 	"os"
 
@@ -12,20 +15,35 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
+// Start script
+const userdata = `#! /bin/bash
+sudo echo "BEGIN USER DATA EXECUTION"
+sudo netstat -tulnp
+sudo pwd
+sudo ls
+sudo echo "HOME=$HOME"
+sudo echo $HOME
+sudo /home/ubuntu/echoserver &
+sudo echo "END USER DATA EXECUTION"`
+
+// i-0582a359d2b760659
+
 func main() {
 	sess := session.Must(session.NewSession())
 	region := aws.String(os.Getenv("AWS_REGION"))
 
 	svc := ec2.New(sess, &aws.Config{Region: region})
+	startscript := base64.StdEncoding.EncodeToString([]byte(userdata))
 
 	params := &ec2.RunInstancesInput{
-		KeyName: aws.String("aws-eb3"),
-		DryRun: aws.Bool(true),
-		ImageId: aws.String("ami-86f4dbe6"),
-		MinCount: aws.Int64(1),
-		MaxCount: aws.Int64(1),
-		InstanceType: aws.String("t1.micro"),
-		SecurityGroups: []*string{ aws.String("sdk-connect") },
+		KeyName:        aws.String("aws-eb2"),
+		DryRun:         aws.Bool(true),
+		ImageId:        aws.String("ami-1a1a337a"),
+		MinCount:       aws.Int64(1),
+		MaxCount:       aws.Int64(1),
+		InstanceType:   aws.String("t2.nano"),
+		SecurityGroups: []*string{aws.String("sdk-connect")},
+		UserData:       aws.String(startscript),
 	}
 
 	resp, err := svc.RunInstances(params)
@@ -39,17 +57,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// var raw interface{}
-	// err := json.Unmarshal(resp, &raw)
-
-	// info := raw.(map[string]interface{})
-
 	log.Printf("%+v\n", resp)
-
-	// tagParams := &ec2.createTagsInput{
-	// 	Resources: []*string{
-	// 		aws.String(instanceId),
-	// 	},
-	// 	Tags: 
-	// }
 }
