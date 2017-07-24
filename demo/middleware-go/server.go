ec03dbfeaa2934ee523f4046f0a867ee029d2241
mtso <mtso@tuta.io>
2017-07-24 14:04:30 -0700

    Demo middleware applier in Go

--- demo/middleware-go/server.go
+++ demo/middleware-go/server.go
@@ -28,16 +28,59 @@ func logStatus(handler http.HandlerFunc) http.HandlerFunc {
 		// Initialize logWriter.
 		lw := &logWriter{w, http.StatusOK}
 
+		log.Println("log before")
 		// Pass logWriter into handler.
 		handler(lw, r)
 
+		log.Println("log after")
 		log.Println(lw.statusCode, r.URL.Path)
 	})
 }
 
+// Attaches a header "foo: bar" to the outgoing response.
+func attachHeader() func(http.HandlerFunc) http.HandlerFunc {
+	return func(handler http.HandlerFunc) http.HandlerFunc {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("foo", "bar")
+			log.Println("attach before")
+			handler(w, r)
+			log.Println("attach after")
+		})
+	}
+}
+
+type middleware func(http.HandlerFunc) http.HandlerFunc
+
+func applyMiddlewares(middlewares ...middleware) middleware {
+	if len(middlewares) < 0 {
+		return func(h http.HandlerFunc) http.HandlerFunc {
+			return h
+		}
+	}
+
+	return func(h http.HandlerFunc) http.HandlerFunc {
+		count := len(middlewares)
+		for i := range middlewares {
+			// Apply in reverse order (first middleware becomes outer-most layer).
+			h = middlewares[count-1-i](h)
+		}
+		return h
+	}
+}
+
+type MiddlewareHandlerFunc struct {
+	handler http.HandlerFunc
+}
+
+func (mh *MiddlewareHandlerFunc) apply(m middleware) *MiddlewareHandlerFunc {
+	mh.handler = m(mh.handler)
+	return mh
+}
+
 func main() {
-
-	http.HandleFunc("/", logStatus(handler))
-
+	h := &MiddlewareHandlerFunc{handler}
+	h.apply(logStatus).apply(attachHeader())
+	// h := applyMiddlewares(logStatus, attachHeader())(handler)
+	http.HandleFunc("/", h.handler)
 	http.ListenAndServe(":3750", nil)
 }
