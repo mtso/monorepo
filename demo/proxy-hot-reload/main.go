bcf0a2448e760f506e681fb64dc62398348be31b
mtso <mtso@tuta.io>
2017-07-27 09:50:16 -0700

    Add reloading from conf file

--- demo/proxy-hot-reload/main.go
+++ demo/proxy-hot-reload/main.go
@@ -1,57 +1,94 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
 	"net/http"
 	"strings"
 
 	"github.com/gorilla/mux"
 )
 
-type Flipper struct {
+type Reloader struct {
 	router *mux.Router
 }
 
-func (f *Flipper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	f.router.ServeHTTP(w, r)
-	if r.URL.Path == "/flip" {
-		f.router = NewRouter(handlers2)
-	} else {
-		f.router = NewRouter(handlers1)
+type Routes map[string]string
+
+var conf = flag.String("conf", "./proxy.conf", "Proxy configuration location.")
+
+func (r *Reloader) Reload() error {
+	raw, err := ioutil.ReadFile(*conf)
+	if err != nil {
+		return err
 	}
-
-	f.router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-		fmt.Println(route.GetPathTemplate())
-		return nil
-	})
+	var routes Routes
+	err = json.Unmarshal(raw, &routes)
+	if err != nil {
+		return err
+	}
+	r.router = NewRouter(routes)
+	return nil
 }
 
-var handlers2 = map[string]http.HandlerFunc{
-	"flop": func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte("<pre>go to /flip</pre>"))
-	},
+func (rl *Reloader) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/reload" {
+		if err := rl.Reload(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		} else {
+			w.Write([]byte("Reloaded successfully"))
+		}
+		return
+	}
+	rl.router.ServeHTTP(w, r)
 }
 
-var handlers1 = map[string]http.HandlerFunc{
-	"flip": func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte("<pre>go to /flop</pre>"))
-	},
-}
+// func (f *Flipper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	f.router.ServeHTTP(w, r)
+// 	if r.URL.Path == "/flip" {
+// 		f.router = NewRouter(handlers2)
+// 	} else {
+// 		f.router = NewRouter(handlers1)
+// 	}
+//
+// 	f.router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+// 		fmt.Println(route.GetPathTemplate())
+// 		return nil
+// 	})
+// }
+
+// var handlers2 = map[string]http.HandlerFunc{
+// 	"flop": func(w http.ResponseWriter, r *http.Request) {
+// 		w.Header().Set("Content-Type", "text/html")
+// 		w.Write([]byte("<pre>go to /flip</pre>"))
+// 	},
+// }
+//
+// var handlers1 = map[string]http.HandlerFunc{
+// 	"flip": func(w http.ResponseWriter, r *http.Request) {
+// 		w.Header().Set("Content-Type", "text/html")
+// 		w.Write([]byte("<pre>go to /flop</pre>"))
+// 	},
+// }
 
 func main() {
-	flipper := &Flipper{NewRouter(handlers2)}
+	flipper := &Reloader{NewRouter(map[string]string{
+		"foo": "bar",
+	})}
 	// router = NewRouter(handlers2)
 	http.ListenAndServe(":3750", flipper)
 }
 
-func NewRouter(routes map[string]http.HandlerFunc) *mux.Router {
+func NewRouter(routes map[string]string) *mux.Router {
 	r := mux.NewRouter()
 
-	for route, handler := range routes {
+	for route, destination := range routes {
 		route = strings.Trim(route, "/")
-		r.PathPrefix("/" + route).HandlerFunc(handler)
+		r.PathPrefix("/" + route).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// temp handler for test
+			w.Write([]byte("forward to " + destination))
+		})
 	}
 
 	return r
