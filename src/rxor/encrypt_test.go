d232b343aa6609374d8aec308d499cc433fee64c
mtso <mtso@tuta.io>
2017-08-22 20:58:52 -0700

    Add repeating-xor program to src as 'rxor' for installation

---
+++ src/rxor/encrypt_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/hex"
+	"testing"
+)
+
+func TestEncrypt(t *testing.T) {
+	want := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+	in := `Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal`
+
+	got := Key("ICE").Encrypt([]byte(in))
+
+	if hex.EncodeToString(got) != want {
+		t.Errorf("Expected %s, but got %s", want, got)
+	}
+}
